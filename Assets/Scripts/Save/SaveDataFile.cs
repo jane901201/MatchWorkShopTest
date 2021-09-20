7137b8e5a80db854430263186d5c02b78e5e0dce
jane36742715 <jane36742715@gmail.com>
2021-09-21 04:10:14 +0800

    2021/9/21-resource input

---
+++ Assets/Scripts/Save/SaveDataFile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Save
+{
+    public class SaveDataFile : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UITool/LongPressButton.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LongPressButton : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+}
